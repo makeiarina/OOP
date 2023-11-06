76d27750e321dee323edd92ae7385bea11da1ceb
Makei Arina <makejarina@gmail.com>
2023-11-06 18:50:43 +0300

    второе задание

--- лр9/зад2.py
+++ лр9/зад2.py
@@ -1,25 +1,27 @@
 #Определение класса Book с атрибутами author, title и year. Метод __init__ инициализирует объект класса Book с тремя аргументами author, title и year и устанавливает соответствующие атрибуты.
 class Book:
+    # Метод init инициализирует объект класса Book с тремя аргументами: author, title и year, и устанавливает соответствующие атрибуты объекта
     def __init__(self, author, title, year):
         self.author = author
         self.title = title
         self.year = year
 
-    def get_info(self):
+    def get_info(self):# Метод get_info возвращает информацию о книге в виде строки
         return f"Книга: {self.title}\nАвтор: {self.author}\nГод выпуска: {self.year}"
 
 
-class Magazine:
-    def __init__(self, title, number, year):
+class Magazine:# Определение класса Magazine, который представляет журнал
+    def __init__(self, title, number, year):# Метод init инициализирует объект класса Magazine с тремя аргументами: title, number и year,
+    # и устанавливает соответствующие атрибуты объекта
         self.title = title
         self.number = number
         self.year = year
 
-    def get_info(self):
-        return f"Журнал: {self.title}\nНомер: {self.number}\nГод выпуска: {self.year}"
+    def get_info(self):# Метод get_info возвращает информацию о журнале в виде строки
+        return f"Журнал: {self.title}\nНомер: {self.number}\nГод выпуска: {self.year}"  # включая название, номер и год выпуска
 
-book = Book("Иванов Иван", "Война и мир", 1869)
-magazine = Magazine("National Geographic", 2022, 3)
+book = Book("Иванов Иван", "Война и мир", 1869)# Создание объекта класса Book с аргументами "Иванов Иван", "Война и мир", 1869
+magazine = Magazine("National Geographic", 3, 2022)# Создание объекта класса Magazine с аргументами "National Geographic", 3, 2022
 
 # Полиморфный вызов метода get_info()
 print(book.get_info())

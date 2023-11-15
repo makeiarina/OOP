9c51aa2cc590aec233e4f366db0cf1cd108b87cc
Makei Arina <makejarina@gmail.com>
2023-11-15 21:26:43 +0300

    третье задание

--- лр6/зад3.py
+++ лр6/зад3.py
@@ -1,8 +1,8 @@
 class Phone:
     '''Класс, представляющий модель абонента.'''
 
-    def __init__(self, last_name, first_name, middle_name, address, number, time_of_intra_city_conversations,
-                 time_of_long_distance_conversations):
+    def init(self, last_name, first_name, middle_name, address, number, time_of_intra_city_conversations,
+             time_of_long_distance_conversations):
         self.last_name = last_name
         self.first_name = first_name
         self.middle_name = middle_name
@@ -11,6 +11,8 @@ class Phone:
         self.time_of_intra_city_conversations = time_of_intra_city_conversations
         self.time_of_long_distance_conversations = time_of_long_distance_conversations
 
+    # Определяется метод для инициализации объекта класса `Phone`.
+
     def print_info(self):
         '''Метод для вывода информации об абоненте.'''
         print(f'ФИО: {self.last_name} {self.first_name} {self.middle_name}')
@@ -18,5 +20,5 @@ class Phone:
         print(f'Номер: {self.number}')
         print(f'Время внутригородних разговоров: {self.time_of_intra_city_conversations}')
         print(f'Время междугородних разговоров: {self.time_of_long_distance_conversations}')
-
+    # Определяется метод для вывода информации об абоненте, использующий атрибуты объекта класса `Phone`.
 

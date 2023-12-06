bd7aa439655f4d862ccd12c60e4f5f65458f1165
Makei Arina <makejarina@gmail.com>
2023-12-06 15:44:52 +0300

    третье задание

--- лр13/зад3.py
+++ лр13/зад3.py
@@ -10,12 +10,13 @@ def close_window():
 # Создаем главное окно приложения.
 window = Tk()
 window.title("Брось кубик")
+window.configure(background="lightblue") # Задаем цвет фона окна
 # Создаем Label с текстом "Брось кубик" и устанавливаем шрифт Arial размером 14.
-label = Label(window, text="Брось кубик", font=("Arial", 14))
+label = Label(window, text="Брось кубик", font=("Arial", 14),fg="indigo")
 label.pack(pady=20)
 # Создаем Button с текстом "Бросить кубик" и устанавливаем шрифт Arial размером 14.
 # При нажатии на кнопку будет вызываться функция roll_dice().
-button = Button(window, text="Бросить кубик", font=("Arial", 14), command=roll_dice)
+button = Button(window, text="Бросить кубик", font=("Arial", 14), command=roll_dice,bg="lightgreen")
 button.pack(pady=10)
 # Запускаем главный цикл приложения.
 window.mainloop()

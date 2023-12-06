503cb3c8eebcb8fcfd01e8394a9262ec3c7ea576
Makei Arina <makejarina@gmail.com>
2023-12-06 15:44:35 +0300

    второе задание

--- лр13/зад2.py
+++ лр13/зад2.py
@@ -9,13 +9,13 @@ window=Tk()#создает главное окно приложения
 window.title('Проект 2')#устанавливает заголовок главного окна
 window.geometry('400x100')#устанавливает размеры главного окна
 
-lab=Label(window,text="",font=('Arial',14))#создает Label с пустым текстом и устанавливает шрифт Arial размером 14
+lab=Label(window,text="",font=('Arial',14),bg='lightyellow')#создает Label с пустым текстом и устанавливает шрифт Arial размером 14
 lab.grid(column=1,row=0)#размещает Label в главном окне в первом столбце и первой строке
 
-btn=Button(window,text='Приветствие',font=('Arial',14),command=clicked)#создает Button с текстом “Приветствие” и устанавливает шрифт Arial размером 14. При нажатии на кнопку будет вызываться функция clicked()
+btn=Button(window,text='Приветствие',font=('Arial',14),command=clicked, bg='lightgreen')#создает Button с текстом “Приветствие” и устанавливает шрифт Arial размером 14. При нажатии на кнопку будет вызываться функция clicked()
 btn.grid(column=0, row=1)#размещает Button в главном окне в первом столбце и второй строке
 
-btn1=Button(window,text='Закрыть',font=('Arial',14),command=close_window)#создает Button с текстом “Закрыть” и устанавливает шрифт Arial размером 14. При нажатии на кнопку будет вызываться функция close_window()
+btn1=Button(window,text='Закрыть',font=('Arial',14),command=close_window, bg='lightcoral')#создает Button с текстом “Закрыть” и устанавливает шрифт Arial размером 14. При нажатии на кнопку будет вызываться функция close_window()
 btn1.grid(column=2,row=1)#размещает Button в главном окне в третьем столбце и второй строке
 
 window.mainloop()#запускает главный цикл приложения, который обрабатывает события и отображает графический интерфейс

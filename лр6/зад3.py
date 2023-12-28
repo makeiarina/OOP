873d24172876e00a8dc89a9af295d91ec783de3a
Makei Arina <makejarina@gmail.com>
2023-12-28 10:23:00 +0300

    третье задание

--- лр6/зад3.py
+++ лр6/зад3.py
@@ -1,8 +1,20 @@
 class Phone:
     '''Класс, представляющий модель абонента.'''
 
-    def init(self, last_name, first_name, middle_name, address, number, time_of_intra_city_conversations,
+    def __init__(self, last_name, first_name, middle_name, address, number, time_of_intra_city_conversations,
              time_of_long_distance_conversations):
+        '''
+        Метод для инициализации объекта класса Phone.
+
+        Параметры:
+        last_name (str): Фамилия абонента.
+        first_name (str): Имя абонента.
+        middle_name (str): Отчество абонента.
+        address (str): Адрес абонента.
+        number (str): Номер телефона абонента.
+        time_of_intra_city_conversations (str): Время внутригородних разговоров абонента.
+        time_of_long_distance_conversations (str): Время междугородних разговоров абонента.
+        '''
         self.last_name = last_name
         self.first_name = first_name
         self.middle_name = middle_name
@@ -11,8 +23,6 @@ class Phone:
         self.time_of_intra_city_conversations = time_of_intra_city_conversations
         self.time_of_long_distance_conversations = time_of_long_distance_conversations
 
-    # Определяется метод для инициализации объекта класса `Phone`.
-
     def print_info(self):
         '''Метод для вывода информации об абоненте.'''
         print(f'ФИО: {self.last_name} {self.first_name} {self.middle_name}')
@@ -20,5 +30,5 @@ class Phone:
         print(f'Номер: {self.number}')
         print(f'Время внутригородних разговоров: {self.time_of_intra_city_conversations}')
         print(f'Время междугородних разговоров: {self.time_of_long_distance_conversations}')
-    # Определяется метод для вывода информации об абоненте, использующий атрибуты объекта класса `Phone`.
+        '''Метод для вывода информации об абоненте, использующий атрибуты объекта класса Phone.'''
 
